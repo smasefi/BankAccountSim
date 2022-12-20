37a00498d00d3b7e8f714f3e2ee85a3534b05032
smasefi <samanthamilan@gmail.com>
2022-12-20 12:52:41 -0500

    updated main menu

--- banking/src/bankSim/BankDirectory.java
+++ banking/src/bankSim/BankDirectory.java
@@ -3,6 +3,7 @@ package bankSim;
 import java.util.*;
 
 public class BankDirectory {
+    static boolean accountStatus = true;
     public static void main(String[] args) {
         System.out.println("Welcome to the Bank Home Page \n");
 
@@ -13,12 +14,31 @@ public class BankDirectory {
 
         System.out.println("Select a Mobile Banking Option: \n" + options);
 
-        while(true){
+        while(accountStatus){
             Scanner scanner = new Scanner(System.in);
             int choice = 0;
             choice = scanner.nextInt();
             if(choice < 1 || choice > 4){
                 System.out.println("The choice you have selected is not present on the menu ");
+            }
+            if(choice == 1){
+                // take to the account
+                System.out.println("Account Amount");
+            }
+            if(choice == 2){
+                // take to class that allows withdrawal
+                System.out.println("Withdraw");
+            }
+
+            if(choice == 3){
+                // take to class that deposits funds
+                System.out.println("Deposit Funds");
+            }
+
+            if(choice == 4){
+                //logout
+                System.out.println("Logout");
+                accountStatus = false;
             }
 
         }
